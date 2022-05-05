c3753a329fe769e26db54c50b0f8f6715cdc777f
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 01:21:43 +0300

    fixing build

---
+++ config/project.py
@@ -0,0 +1,4 @@
+project_name = "myworld-python"
+project_long_description = "myworld system implemented in python"
+project_year_started = 2016
+project_description = """myworld system implemented in python"""
