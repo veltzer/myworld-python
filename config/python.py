c0a878e794a35eaf1beafda90a4464874dffe9fb
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 14:10:50 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -2,22 +2,15 @@ import config.project
 
 package_name = config.project.project_name
 
-console_scripts = []
-setup_requires = []
 run_requires = [
-    'google-api-python-client',
-    'click',
+    "google-api-python-client",
+    "click",
 ]
-test_requires = []
 dev_requires = [
     "pymakehelper",
     "pydmt",
     "pyclassifiers",
 ]
-install_requires = list(setup_requires)
-install_requires.extend(run_requires)
-
-extras_require = {}
 
 python_requires = ">=3.9"
 test_os = ["ubuntu-20.04"]
