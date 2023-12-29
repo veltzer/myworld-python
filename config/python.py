14851730ecfd6384fdda9862e99ea3a55f619f54
Mark Veltzer <mark.veltzer@gmail.com>
2023-12-29 20:56:18 +0200

    move to new pydmt

--- config/python.py
+++ config/python.py
@@ -1,9 +1,13 @@
+config_requires = []
+dev_requires = []
+install_requires = [
+    "google-api-python-client",
+    "click",
+]
 make_requires = [
     "pymakehelper",
     "pydmt",
     "pyclassifiers",
 ]
-install_requires = [
-    "google-api-python-client",
-    "click",
-]
+test_requires = []
+requires = config_requires + install_requires + make_requires + test_requires
