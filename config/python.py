d82c139ff9a7dfb5fbcb4a199e553bed2dfba017
Mark Veltzer <mark.veltzer@gmail.com>
2024-01-12 15:06:49 +0200

    more python.py changes

--- config/python.py
+++ config/python.py
@@ -4,10 +4,10 @@ install_requires = [
     "google-api-python-client",
     "click",
 ]
-make_requires = [
+build_requires = [
     "pymakehelper",
     "pydmt",
     "pyclassifiers",
 ]
 test_requires = []
-requires = config_requires + install_requires + make_requires + test_requires
+requires = config_requires + install_requires + build_requires + test_requires
