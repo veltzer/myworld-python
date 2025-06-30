1c8e9738d09bcc7a436616a9083673a933390e72
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-30 14:09:49 +0300

    fixed up deps

--- config/python.py
+++ config/python.py
@@ -1,11 +1,11 @@
 """ python deps for this project """
 
+import config.shared
+
 install_requires: list[str] = [
     "google-api-python-client",
     "click",
 ]
-build_requires: list[str] = [
-    "pydmt",
-    "pymakehelper",
-]
-requires = install_requires + build_requires
+build_requires: list[str] = config.shared.BUILD
+test_requires: list[str] = config.shared.TEST
+requires = install_requires + build_requires + test_requires
